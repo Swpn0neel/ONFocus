6f289a12240487e6c62b5db9f78ca93387b1d517
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-07-25 23:57:13 +0530

    Created CustomTime.jsx component
    It will allow the users to set custom times for themselves (as required).

---
+++ ONFocus/src/Components/CustomTime.jsx
@@ -0,0 +1,27 @@
+import { Menu } from "@mantine/core";
+import Settings from "../assets/Settings.svg";
+
+export default function CustomTime({ handleTimeSelect, selectedTime }) {
+  const timerOptions = [1, 2, 5, 10, 15, 20, 25];
+  return (
+    <Menu width={200} shadow="md">
+      <Menu.Target>
+        <button>
+          <img src={Settings} className="h-8" alt="" />
+        </button>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        {timerOptions.map((option) => (
+          <Menu.Item
+            key={option}
+            onClick={() => handleTimeSelect(option)}
+            className={selectedTime === option ? "active" : ""}
+          >
+            {option} minute{option === 1 ? "" : "s"}
+          </Menu.Item>
+        ))}
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
